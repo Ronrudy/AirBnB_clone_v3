4f1d11d50cd59f007190f934727da5629121d572
Ronrudy <ruffroony@yahoo.com>
2024-04-01 05:47:12 -0700

    still trying

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/pytihon3
 """
 index
 """
@@ -7,6 +7,12 @@ from flask import jsonify
 from api.v1.views import app_views
 
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
